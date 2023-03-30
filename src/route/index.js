54339f14337ce236d1537f5be4393f32a43820af
ann1205 <gannanychyporchuk@gmail.com>
2023-03-30 12:36:07 +0300

    done 4 lessons

--- src/route/index.js
+++ src/route/index.js
@@ -3,6 +3,37 @@ const express = require('express')
 // Cтворюємо роутер - місце, куди ми підключаємо ендпоїнти
 const router = express.Router()
 
+// ===============================================================
+
+var header = {
+  name: {
+    firstname: 'Dmytro',
+    lastname: 'Ivanov',
+  },
+  position: 'Junior Fullstack JS Developer',
+  salary: '600$ в місяць',
+  address: 'Ukraine, Lviv',
+};
+
+var footer = {
+  social: {
+    email: {
+      text: 'dmytro@mail.com',
+      href: 'mailto:dmytro@mail.com',
+    },
+
+    phone: {
+      text: '+380670000123',
+      href: 'tel:+380670000123',
+    },
+
+    linkedin: {
+      text: 'LinkedIn',
+      href: 'https://www.linkedin.com/in/dmytro-test',
+    },
+  },
+};
+
 // ================================================================
 
 // router.get Створює нам один ентпоїнт
@@ -23,10 +54,224 @@ router.get('/summary', function (req, res) {
   //             ↙ cюди вводимо назву файлу з сontainer
   res.render('summary', {
     // ↙ сюди вводимо JSON дані
+
+    page: {
+        title: 'Resume |  Summary',
+    },
+
+    header,
+
+   
+    main: {
+      summary: {
+        title: 'Summary',
+        text: 'Open-minded for new technologies, with 1 years of experience in development. Whenever I start to work on a new project I learn the domain and try to understand the idea of the project. Good team player, every colleague is a friend to me.',
+      },
+
+      experience: {
+        title: 'Other experience',
+        text: `Pet project for parsing sport betting data from different platforms ( odds ) and sport statistics (
+          tournament position, goals etc), analyzing by simple mathematics models and preparing probability
+          for such events like: money line - first win / draw / second win, totals etc.`,
+      },
+    },
+
+    footer,
   })
 })
 
 // ================================================================
+
+router.get('/skills', function (req, res) {
+  //             ↙ cюди вводимо назву файлу з сontainer
+  res.render('skills', {
+    // ↙ сюди вводимо JSON дані
+
+    page: {
+      title: 'Resume  |  Skills',
+    },
+
+    header,
+   
+    main: {
+                
+        skills: [
+          {
+            name: 'HTML',
+            point: 10,
+            isTop: true,
+          },
+          {
+            name: 'Handlebars',
+            point: 8,
+          },
+          {
+            name: 'VS Code',
+            point: 9,
+          },
+          {
+            name: 'Git',
+            point: 6,
+          },
+          {
+            name: 'Terminal',
+            point: 10,
+            isTop: true,
+          },
+          {
+            name: 'NPM',
+            point: 5,
+          },
+          {
+            name: 'React.js',
+            point: 0,
+          },
+          {
+            name: 'PHP',
+            point: null,
+          },                 
+        ],
+        
+        hobbies: [
+          {
+            name: 'Drive a car',
+            isMain: true,
+          },
+          {
+            name: 'Fishing',
+            isMain: false,
+          },
+          {
+            name: 'Shopping',
+            isMain: true,
+          },
+        ],
+      },
+
+    footer,
+  })
+})
+
+router.get('/education', function (req, res) {
+    //             ↙ cюди вводимо назву файлу з сontainer
+    res.render('education', {
+      // ↙ сюди вводимо JSON дані
+  
+      page: {
+        title: 'Resume  |  Education',
+    },
+
+    header,
+
+   
+    main: {
+     educations: [
+      {
+        name: 'IT STEP University',
+        isEnd: true,
+      },
+      {
+        name: 'Lviv National University of Ivan Franko',
+        isEnd: false,
+      },
+      {
+        name: 'The National Forestry University of Ukraine',
+        isEnd: true,
+      },
+      {
+        name: 'Lviv Polytechnic',
+        isEnd: false,
+      },      
+    ],
+    certificates: [
+      {
+        name: 'Свідоцтво про закінчення IT STEP University',
+        id: 1236513,
+      },
+      {
+        name: 'Диплом магістра',
+        id: 86945464,
+      },
+      {
+        name: 'Диплом бакалавра',
+        id: 615163513131,
+      },
+    ]
+    },
+
+    footer,
+            
+    })
+  })
+  
+  router.get('/work', function (req, res) {
+    //             ↙ cюди вводимо назву файлу з сontainer
+    res.render('work', {
+      // ↙ сюди вводимо JSON дані
+
+      layout: 'big',
+  
+      page: {
+        title: 'Resume  |  Work',
+    },
+
+    header,
+
+   
+    main: {
+      works: [
+        {
+          position: 'Junior Fullstack Developer',
+          company: {
+            name: 'IT Brains',
+            url: 'https://it-brains.com.ua/',
+          },
+          duration: {
+            from: '10.10.2022',
+            to: null
+          },
+          projectAmount: 3,
+
+          projects: [{
+            name: 'Resume',
+            url: 'https://resume.com.ua/',
+            about: 'Airbnb competitor. High-load application for searching apartments',
+
+            stacks: [
+              {
+                name: 'React.js',
+              },
+              {
+                name: 'HTML / CSS',
+              },
+              {
+                name: 'Nodejs',
+              },
+            ],
+
+            stackAmount: 10,
+
+            awards: [
+              {
+                name: 'Background verification - is a feature that provides users to prove that they are real person.'
+              },
+              {
+                name: 'Preparing SEO optimized pages. The automated process of getting data for the app from documents.'
+              },
+            ],
+
+            awardAmount: 5,
+          },
+        ],
+        },
+      ],
+    },
+
+    footer,
+            
+    })
+  })
+
 
 // Підключаємо роутер до бек-енду
 module.exports = router
